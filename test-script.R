731eba8a5e0a17badd281838f07d2ff815313693
bittnation <dramaticrachel@gmail.com>
2025-05-15 16:17:02 -0700

    changing colors in visualization

--- test-script.R
+++ test-script.R
@@ -1,7 +1,11 @@
 library(tidyverse)
 library(lterdatasampler)
+library(ggplot2)
 
 ggplot(data = pie_crab,
        aes(x = lattitude,
-           y=size))+
-  geom_point()
+           y=size,
+           color= latitude))+
+  geom_point()+
+  scale_color_gradient(low = blue,
+                       high = green)
